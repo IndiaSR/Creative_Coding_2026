4272e9b98487151c69faf7e84dacb70ba6f45b59
India Stroh-Reilly <S4010028@student.rmit.edu.au>
2026-05-21 19:09:01 +1000

    Add files via upload

---
+++ 02_Text_To_Point_Lines/Text_To_Point_Lines_8/Text_To_Point_Lines_8.js
@@ -0,0 +1,71 @@
+// Text to point lines variation 08
+// Change = removed ellipses entirely, left with the outlines from the lines
+// kept the lines still and changed colour to blue
+
+let myFont;
+let points;
+let sampleF;
+
+// preload() = anything to download. Downloading the typeface first
+function preload() {
+  myFont = loadFont("data/AdobeArabic-BoldItalic.otf");
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  // This value controls how many points are generated on the text outline.
+  sampleF = 0.2;
+
+  // Extract our array of outline points!
+  points = myFont.textToPoints('Melbourne', (width / 4) - 200, height / 2, 300, {
+    sampleFactor: sampleF,
+    simplifyThreshold: 0
+  });
+
+}
+
+function draw() {
+  
+  background(10,0,10); 
+  
+  //////CHANGE
+  //let maxReach = 400; // max distance at which lines appear
+
+  // A 'for' loop lets us iterate over every single point inside our 'points' array
+  for (let i = 0; i < points.length; i++) {
+    let p = points[i];
+    //let d = dist(p.x,p.y,mouseX,mouseY);
+    //if (d > maxReach) continue;
+    
+    //let alpha = map(d, 0, maxReach, 220, 0); // VSC assistance
+    //let weight = map(d, 0, maxReach, 4, 0.5);
+    
+    //let dotSize = map(d, 0, maxReach, 14, 4);
+    stroke(100,100,255);
+    strokeWeight(0.3);
+    line(p.x, p.y,width/2,height/6);
+
+    //fill(300,300,0,alpha);
+    //noStroke();
+    //strokeWeight(1);
+
+    // Draw the dot exactly at the mathematical coordinate
+    //ellipse(p.x, p.y, 8, 8);
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  points = myFont.textToPoints('Melbourne', (width / 4) - 200, height / 2, 300, {
+    sampleFactor: sampleF,
+    simplifyThreshold: 0
+    });
+}
+  
+  //save as GIF
+function keyPressed() {
+  if (key === 's') {
+    saveGif('Text_To_Point_Lines_8', 5); // Saves 5 seconds of animation
+  } 
+}
