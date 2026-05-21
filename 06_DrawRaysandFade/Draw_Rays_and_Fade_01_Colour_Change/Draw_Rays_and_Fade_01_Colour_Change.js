1fcd4788b4a58bb47ebaa59802cb5881d8fb7d3a
India Stroh-Reilly <S4010028@student.rmit.edu.au>
2026-05-21 20:42:29 +1000

    Add files via upload

---
+++ 06_DrawRaysandFade/Draw_Rays_and_Fade_01_Colour_Change/Draw_Rays_and_Fade_01_Colour_Change.js
@@ -0,0 +1,49 @@
+//Draw Rays and Fade Variation 01
+//Colour Change 
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  
+  background(255, 0, 255); 
+}
+
+function draw() {
+//4th value is opacity which creates the fading effect 
+  background(255, 0, 255, 6);
+
+  if (mouseIsPressed) {
+    // If the mouse is pressed, colour of circle changes
+    fill(0, 0, 255);
+    stroke(255, 255, 255);
+    
+    // 💡 TRY THIS: Uncomment the line below to randomize the background color when clicked!
+    // background(random(255), random(255), random(255)); 
+  } else {
+
+    fill("pink"); 
+    stroke(0);      
+  }
+  
+  strokeWeight(2);
+  
+// CIRCLE
+  circle(mouseX, mouseY, 64);
+  
+  // "rays" extending from the center out to the cursor 
+   //circle(width / 2, height / 2, 24);
+   //line(width / 2, height / 2, mouseX, mouseY);  
+}
+
+
+// Save 5-second gif
+function keyPressed() {
+  if (key === 's') {
+    saveGif('Draw_Rays_and_Fade_01_Colour_Change.gif', 5);
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  background(255, 255, 0); 
+}
